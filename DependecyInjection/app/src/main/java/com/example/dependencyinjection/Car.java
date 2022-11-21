98dec271d1d250edf3f80b1cda238b8b6f5e885d
VinayTShetty <vinaytshetty@gmail.com>
2022-11-21 22:36:49 +0530

    Method Injection. Added Points :- Why the injection 'remote.CarRemoteConnectes(this)' is used the method,rather than constructor.

--- DependecyInjection/app/src/main/java/com/example/dependencyinjection/Car.java
+++ DependecyInjection/app/src/main/java/com/example/dependencyinjection/Car.java
@@ -26,7 +26,10 @@ public class Car {
      * 2) Next Filed Injection will be completed.i.e Engine Object
      * 3) Method injection will be completed.
      *
-     * Note:- This method will be executed automatically from dagger.
+     * Note:-
+     * This method will be executed automatically from dagger.
+     * In this method Its possible to use 'remote.CarRemoteConnectes(this)' inside the constructor only.
+     * But the constructor Object will not be fully completed to use it.So Using it inside the method.
      */
     @Inject
     public void ConnectRemote(Remote remote) {
